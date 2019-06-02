782a02447de95b9f571975efa3dac729c460870b
dnsprogress <dani@canisnfelis.com>
2019-06-02 09:04:29 +0700

    completed products

--- controllers/product.js
+++ controllers/product.js
@@ -6,29 +6,35 @@ const productModel = require("../models/product");
 
 const tokenHelper = require("../helpers/token");
 
+router.get("/", (req, res) => {
+    let sort = req.query.sort;
+    let page = parseInt(req.query.page);
+    let limit = parseInt(req.query.limit);
+    let preOffset = page * limit;
+    let offset = preOffset - limit;
+    console.log(offset);
+    productModel.getAll(sort, limit, offset, (results) => {
+        res.status(200).json({
+            status : true,
+            data : results
+        })
+    })
+})
+
 router.get("/:id", (req, res) => {
-    tokenHelper.verifyToken(req.headers.token, (callback) => {
-        if(callback == "valid") {
-            productModel.getById(req.params.id, (results) => {
-                if(results.product) {
-                    res.status(200).json({
-                        status : true,
-                        data : results
-                    })
-                } else {
-                    res.status(404).json({
-                        status : false,
-                        message : "product not found"
-                    })
-                }
-            });
+    productModel.getById(req.params.id, (results) => {
+        if(results.product) {
+            res.status(200).json({
+                status : true,
+                data : results
+            })
         } else {
-            res.status(400).json({
+            res.status(404).json({
                 status : false,
-                message : "invalid token"
+                message : "product not found"
             })
         }
-    })
+    });
 })
 
 module.exports = router;
--- models/product.js
+++ models/product.js
@@ -1,27 +1,75 @@
 const database  = require("../database");
 
-const databasePromise = require("../database-promise");
-
 module.exports = {
     getById : (id, callback) => {
-        let product, productSpecification;
-        let query = `SELECT * FROM product WHERE id=?`;
-        databasePromise.query(query, [id])
+        let product, productSpecification, productAttribute, productImage;
+        let query = `SELECT *, 
+                    (SELECT COUNT(*) FROM product_review WHERE 
+                    product_review.product_id = product.id) as review,
+                    (SELECT COUNT(*) FROM product_wishlist WHERE
+                    product_wishlist.product_id = product.id) as wishlist,
+                    (SELECT COUNT(*) FROM product_discussion WHERE
+                    product_discussion.product_id = product.id) as discussion,
+                    (SELECT ROUND(AVG(rating),1) FROM product_rating WHERE
+                    product_rating.product_id = product.id) as rating
+                    FROM product WHERE id=?`;
+        database.query(query, [id])
         .then(rows => {
             product = rows;
-            return databasePromise.query(`SELECT * FROM product_specification 
+            return database.query(`SELECT * FROM product_specification 
             WHERE product_id=?`, [id])
         })
         .then(rows => {
             productSpecification = rows
-            return productSpecification;
+            return database.query(`SELECT * FROM product_attribute 
+            WHERE product_id=?`, [id]);
+        })
+        .then(rows => {
+            productAttribute = rows;
+            return database.query(`SELECT * FROM product_image 
+            WHERE product_id=?`, [id]);
+        })
+        .then(rows => {
+            productImage = rows;
+            return productImage;
         })
         .then(() => {
             let obj = {
                 product : product[0],
-                productSpecification : productSpecification
+                productSpecification : productSpecification,
+                productAttribute : productAttribute,
+                productImage : productImage
             }
             callback(obj)
+        })
+    },
+
+    getAll : (sort, limit, offset, callback) => {
+        let query;
+        if(sort == "popular") {
+            query = `SELECT *, (SELECT ROUND(AVG(rating),1) FROM product_rating WHERE
+                    product_rating.product_id = product.id) as rating FROM product
+                    ORDER BY view DESC LIMIT ? OFFSET ?`
+        } else if(sort == "latest") {
+            query = `SELECT *, (SELECT ROUND(AVG(rating),1) FROM product_rating WHERE
+                    product_rating.product_id = product.id) as rating FROM product
+                    ORDER BY created_at DESC LIMIT ? OFFSET ?`
+        } else if(sort == "cheapest") {
+            query = `SELECT *, (SELECT ROUND(AVG(rating),1) FROM product_rating WHERE
+                    product_rating.product_id = product.id) as rating FROM product
+                    ORDER BY price ASC LIMIT ? OFFSET ?`
+        } else if(sort == "expensive") {
+            query = `SELECT *, (SELECT ROUND(AVG(rating),1) FROM product_rating WHERE
+                    product_rating.product_id = product.id) as rating FROM product
+                    ORDER BY price DESC LIMIT ? OFFSET ?`
+        } else {
+            query = `SELECT *, (SELECT ROUND(AVG(rating),1) FROM product_rating WHERE
+                    product_rating.product_id = product.id) as rating FROM product
+                    ORDER BY view DESC LIMIT ? OFFSET ?`
+        }
+        database.query(query, [limit, offset])
+        .then(rows => {
+            callback(rows)
         })
     }
 }
