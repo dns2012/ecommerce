de965971de52f93c930112ef278977606c0898e2
dns2012 <dnsprogress@gmail.com>
2019-06-25 04:04:31 +0700

    config database env

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 const mysql = require('promise-mysql');
 
+require("dotenv").config();
+
 const database = mysql.createPool({
-    host : 'localhost',
-    user : 'root',
-    password : 'Php7.0Native',
-    database : 'ecommerce'
+    host : process.env.DATABASE_HOST,
+    user : process.env.DATABASE_USER,
+    password : process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE_NAME
 })
 
 // database.connect();
